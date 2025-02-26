d7359a5fcdd492b61d49e8c88906924481185610
kuyugama <mail.kuyugama@gmail.com>
2025-02-26 04:02:25 +0200

    Make CollapsibleText support AnnotatedString

--- app/src/main/java/online/nyam/hikka/ui/components/CollapsibleText.kt
+++ app/src/main/java/online/nyam/hikka/ui/components/CollapsibleText.kt
@@ -1,12 +1,12 @@
 package online.nyam.hikka.ui.components
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateIntAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -14,17 +14,21 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import online.nyam.hikka.ui.theme.HikkaTheme
@@ -32,16 +36,39 @@ import online.nyam.hikka.util.toDp
 import kotlin.time.Duration.Companion.seconds
 
 @Composable
-fun CollapsibleText(
-    content: String,
-    style: TextStyle,
+fun animateHeight(
+    targetValue: Int,
+    animationDuration: Int
+): State<Int> =
+    animateIntAsState(
+        targetValue = targetValue,
+        label = "Height Animation",
+        animationSpec = tween(animationDuration, easing = LinearOutSlowInEasing)
+    )
+
+@Suppress("unused")
+class MeasureScope(
+    private val measurer: TextMeasurer,
+    private val style: TextStyle,
+    private val maxLines: Int,
+    private val constraints: Constraints
+) {
+    fun measure(text: String): TextLayoutResult = measurer.measure(text, style = style, maxLines = maxLines, constraints = constraints)
+
+    fun measure(text: AnnotatedString): TextLayoutResult =
+        measurer.measure(text, style = style, maxLines = maxLines, constraints = constraints)
+}
+
+@Composable
+private fun CollapsibleText(
     expanded: Boolean,
+    measure: MeasureScope.() -> TextLayoutResult,
     modifier: Modifier = Modifier,
-    color: Color = Color.Unspecified,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
     expandedLines: Int = Int.MAX_VALUE,
     collapsedLines: Int = 5,
-    onOverflow: (() -> Unit)? = null,
-    animationDuration: Int = 400
+    animationDuration: Int = 400,
+    textComposable: @Composable (Modifier, TextStyle, TextOverflow) -> Unit
 ) {
     val maxLines =
         when {
@@ -49,43 +76,81 @@ fun CollapsibleText(
             else -> collapsedLines
         }
 
-    val measurer = rememberTextMeasurer()
+    BoxWithConstraints(modifier) {
+        val result =
+            MeasureScope(rememberTextMeasurer(), style, maxLines, constraints).run { measure() }
 
-    BoxWithConstraints(
-        modifier = modifier
-    ) {
-        val measurement =
-            remember(content, style, maxLines, constraints) {
-                measurer
-                    .measure(
-                        content,
-                        style,
-                        maxLines = maxLines,
-                        constraints = constraints
-                    )
-            }
-
-        if (measurement.didOverflowHeight) {
-            onOverflow?.invoke()
-        }
-
-        val measuredHeight = measurement.size.height
-
-        val displayHeight by animateIntAsState(
-            targetValue = measuredHeight,
-            label = "Height Animation",
-            animationSpec = tween(animationDuration)
+        val displayHeight by animateHeight(
+            targetValue = result.size.height,
+            animationDuration = animationDuration
         )
 
+        textComposable(
+            Modifier.height(displayHeight.toDp()),
+            style,
+            if (displayHeight !=
+                result.size.height
+            ) {
+                TextOverflow.Clip
+            } else {
+                TextOverflow.Ellipsis
+            }
+        )
+    }
+}
+
+@Composable
+fun CollapsibleText(
+    content: String,
+    expanded: Boolean,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
+    collapsedLines: Int = 5,
+    expandedLines: Int = Int.MAX_VALUE,
+    animationDuration: Int = 400
+) {
+    CollapsibleText(
+        modifier = modifier,
+        style = style,
+        expanded = expanded,
+        measure = { measure(content) },
+        collapsedLines = collapsedLines,
+        expandedLines = expandedLines,
+        animationDuration = animationDuration
+    ) { innerModifier, innerStyle, overflow ->
         Text(
-            content,
-            style = style,
-            color = color,
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .height(displayHeight.toDp()),
-            overflow = if (displayHeight != measuredHeight) TextOverflow.Clip else TextOverflow.Ellipsis
+            text = content,
+            modifier = innerModifier,
+            style = innerStyle,
+            overflow = overflow
+        )
+    }
+}
+
+@Composable
+fun CollapsibleText(
+    content: AnnotatedString,
+    expanded: Boolean,
+    modifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
+    collapsedLines: Int = 5,
+    expandedLines: Int = Int.MAX_VALUE,
+    animationDuration: Int = 400
+) {
+    CollapsibleText(
+        modifier = modifier,
+        style = style,
+        expanded = expanded,
+        measure = { measure(content) },
+        collapsedLines = collapsedLines,
+        expandedLines = expandedLines,
+        animationDuration = animationDuration
+    ) { innerModifier, innerStyle, overflow ->
+        Text(
+            text = content,
+            modifier = innerModifier,
+            style = innerStyle,
+            overflow = overflow
         )
     }
 }
